5437df1f4561e801a54fe51c341aae11e570a3d0
Sir Atops <162988984+Atops93@users.noreply.github.com>
2024-06-07 10:10:05 +0930

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,3 @@ sudo pacman -Syu git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-
-sudo pacman -Syu xorg-server xorg-xinit plasma-desktop neofetch glava firefox virt-manager qemu-full
